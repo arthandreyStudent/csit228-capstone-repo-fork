4d61433b427cf2606a5336200540b3796839f3df
arthandreyStudent <arthandrey.endrina@cit.edu>
2026-05-04 15:07:50 +0800

    chore: clean up JavaFX module setup and document IntelliJ Maven fix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,17 +1,9 @@
 module com.csit228.capstone {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
     requires java.sql;
 
+    exports com.csit228.capstone;
     exports com.csit228.capstone.controller;
     opens com.csit228.capstone.controller to javafx.fxml;
 }
